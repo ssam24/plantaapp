9e3245202462218dd9cbde9a79ce78fc4b1da8c9
samuel crespo <crespoalzatesamuel@gmail.com>
2020-05-28 09:13:29 -0500

    Updates

---
+++ precache-manifest.57b9e63e31d5e26f518f30165744cfb4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c793198cc068d95e3418763a1c8de28",
+    "url": "/plantaapp/index.html"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/plantaapp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "89a37a82c3ea4d2fbe34",
+    "url": "/plantaapp/static/css/main.0a54a213.chunk.css"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/plantaapp/static/js/2.e150fbdd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/plantaapp/static/js/2.e150fbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a37a82c3ea4d2fbe34",
+    "url": "/plantaapp/static/js/main.1048bea4.chunk.js"
+  },
+  {
+    "revision": "abad2299eff388c42ff6",
+    "url": "/plantaapp/static/js/runtime-main.cef481c2.js"
+  },
+  {
+    "revision": "ba631b638722bb02c7a4ee1998ba6224",
+    "url": "/plantaapp/static/media/banner_img.ba631b63.jpg"
+  },
+  {
+    "revision": "0d3d721254ae1141a1533f413fcd768c",
+    "url": "/plantaapp/static/media/logo.0d3d7212.png"
+  }
+]);
